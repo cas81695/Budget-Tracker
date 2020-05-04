7fbe3955a78302a4bdb54081864f0f854063f028
Christian Sanchez <cas81695@gmail.com>
2020-05-04 16:03:57 -0400

    edits in server

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 7000;
+
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
@@ -18,7 +20,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-
 const databaseUrl = process.env.MONGODB_URI || "budget";
 const collections = ["budget"];
 
@@ -29,8 +30,6 @@ db.on("error", error => {
 });
 
 app.use(require("./routes/api.js"));
-
-const PORT = process.env.PORT || 3550;
 
 app.listen(PORT, () => {
   console.log(`Application running on PORT ${PORT}`);
